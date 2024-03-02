81fc2f01777500717475d128c0c2ba37fc3316f6
GMSTER22 <gmster22@gmail.com>
2024-03-02 13:56:42 -0500

    Fix SimpleQueue error

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -54,7 +54,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -66,8 +66,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
